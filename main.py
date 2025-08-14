3d88475e3d2cbd5460c17d4ba37502787c6675de
Maximus-mx <massimo.jinglik@gmail.com>
2025-08-14 15:22:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ import telebot
 from random import randint
 from datetime import datetime
 from telebot import types
+import zequests
+import os
+import gdown
+from flask import Flask, zequests
 
-TOKEN = "7736502837:AAFjenOoOYGkJWjqnlQjkbuJnADr179gyOM"
+TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Bot is running"
+
+
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_update([update])
+    return '', 200
 
 class States:
     SUM_FIRST_NUMBER = "SUM_FIRST_NUMBER"
@@ -156,7 +174,23 @@ def dice_answer(call):
     else:
         bot.send_message(call.message.chat.id, "Попробуй ещё раз")
 
+if __name__ == "main":
+    server_url = os.getenv("RENDER_EXTERNAL_URL")
+    if server_url and TOKEN:
+        webhook_url = f"{server_url}/{TOKEN}"
+        set_webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={webhook_url}"
+        try:
+            r = requests.get(set_webhook_url)
+            print("Webhook установлен:", r.text)
+        except Exception as e:
+            print("Ошибка при установке webhook:", e)
 
-bot.polling(none_stop=True, interval=0)
+        port = int(os.environ.get("PORT", 10000))
+        print(f"Starting server on port {port}")
+        app.run(host='0.0.0.0', port=port)
+    else:
+        print("Запуск бота в режиме pooling")
+        bot.remove_webhook()
+        bot.polling(none_stop=True)
 
 
