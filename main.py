0ad601c17bb6f5154c7b1c490e0a1e184dd94ccc
Maximus-mx <massimo.jinglik@gmail.com>
2025-08-15 14:36:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telebot import types
 import requests
 import os
 import gdown
-from flask import Flask, zequests
+from flask import Flask, requests
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
